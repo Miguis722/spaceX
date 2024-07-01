947851417fa3cfb69537cbdf517903e63bc5a94c
Miguis722 <160697097+Miguis722@users.noreply.github.com>
2024-07-01 11:39:35 -0500

    feat: :sparkles: Pagina Rockets
    Se hace una función para que cuando cargue la pagina de Rockets, se autoclickee la primera opción (boton) para que coja ese primer ID a mostrar info

--- js/module/rockets/main.js
+++ js/module/rockets/main.js
@@ -1,27 +1,28 @@
 import { generateButtons } from './title/title.js';
 import { updateLeftTextsRocket } from './textleft/textleft.js';
 import { updateInformationBelowRockets } from './down-info/down-info.js';
-import { updateImagesRockets } from './images/images.js'; // Importar la función
+import { updateImagesRockets } from './images/images.js';
 
 // Llamar a la función para generar los botones dinámicamente
 generateButtons();
 
-// Llamar a la función para cambiar el texto de la izquierda y actualizar la información inferior e imágenes
-document.addEventListener('DOMContentLoaded', () => {
-    const buttonsContainer = document.getElementById('buttonsContainer'); // Ajusta el ID según tu estructura HTML
+// Llamar a la función para cambiar el texto de la izquierda
+document.addEventListener('DOMContentLoaded', async () => {
+    const buttonsContainer = document.getElementById('buttonsContainer');
     
+    // Listener para actualizar el texto izquierdo cuando se hace clic en los botones
     buttonsContainer.addEventListener('click', async (event) => {
         if (event.target && event.target.nodeName === 'BUTTON') {
             const rocketId = event.target.id; // Obtiene el ID del botón clickeado
-            
-            // Actualizar el texto de la izquierda
-            await updateLeftTextsRocket(rocketId);
-            
-            // Actualizar la información inferior
-            await updateInformationBelowRockets(rocketId);
-            
-            // Actualizar las imágenes
-            await updateImagesRockets(rocketId);
+            await updateLeftTextsRocket(rocketId); // Llama a la función para actualizar el texto izquierdo con el ID del cohete
+            await updateInformationBelowRockets(rocketId); // Llama a la función para actualizar la información inferior con el ID del cohete
+            await updateImagesRockets(rocketId); // Llama a la función para actualizar las imágenes con el ID del cohete
         }
     });
+
+    // Simular el clic en el botón 1 cuando se cargue la página
+    const firstButton = buttonsContainer.querySelector('button'); // Encuentra el primer botón
+    if (firstButton) {
+        firstButton.click();
+    }
 });
