38e8604b1d094dbebd4137332550bcf74c5db5fd
Miguis722 <160697097+Miguis722@users.noreply.github.com>
2024-06-30 20:37:45 -0500

    feat: :sparkles: Botones rockets
    Se hace la funcionalidad de botones interactivos con ID's para rockets

---
+++ js/module/rockets/buttons/buttons.js
@@ -0,0 +1,19 @@
+import { idsbuttonsRockets } from './idsbutton.js';
+
+export function generateButtons() {
+    const buttonsContainer = document.getElementById('buttonsContainer');
+    buttonsContainer.innerHTML = ''; // Limpiar el contenedor de botones
+
+    idsbuttonsRockets.forEach((id, index) => {
+        const anchor = document.createElement('a');
+        anchor.href = '#';
+
+        const button = document.createElement('button');
+        button.className = 'button_main_page';
+        button.id = id;
+        button.textContent = index + 1; // Mostrar el número secuencial
+
+        anchor.appendChild(button);
+        buttonsContainer.appendChild(anchor);
+    });
+}
